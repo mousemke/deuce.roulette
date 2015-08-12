7e36e3ce39d4669549e5c969a5bfea21d4352b75
Mouse Braun <mouse@knoblau.ch>
2015-08-12 09:03:19 +0200

    added unobfuscated script

---
+++ deuce.roulette.js
@@ -0,0 +1,81 @@
+function il1i1il1i1ilii(ili1li1li1liii){iiliili1il1i1i=ili1li1li1liii.match(
+/.{1,4}/g)||[];ili1il1il1i111='';for(ililililililii=0;ililililililii<iiliili1il1i1i.length;
+ililililililii++){ili1il1il1i111+=String.fromCharCode(parseInt(iiliili1il1i1i[
+ililililililii],16));}return ili1il1il1i111;}var ilii111ili11ll,lllliiiliiliil=
+'#efefef',iliilil1ii11ll='#00AC02',liliii111il1ii='#F50F28',ilii1li11lii11='#050706',
+lilililiiliiii = [['0',iliilil1ii11ll],['28',ilii1li11lii11],['9',liliii111il1ii],
+['26',ilii1li11lii11],['30',liliii111il1ii],['11',ilii1li11lii11],['7',liliii111il1ii],
+['20',ilii1li11lii11],['32',liliii111il1ii],['17',ilii1li11lii11],['5',liliii111il1ii],
+['22',ilii1li11lii11],['34',liliii111il1ii],['15',ilii1li11lii11],['3',liliii111il1ii],
+['24',ilii1li11lii11],['36',liliii111il1ii],['13',ilii1li11lii11],['1',liliii111il1ii],
+['00',iliilil1ii11ll],['27',liliii111il1ii],['10',ilii1li11lii11],['25',liliii111il1ii],
+['29',ilii1li11lii11],['12',liliii111il1ii],['8',ilii1li11lii11],['19',liliii111il1ii],
+['31',ilii1li11lii11],['18',liliii111il1ii],['6',ilii1li11lii11],['21',liliii111il1ii],
+['33',ilii1li11lii11],['16',liliii111il1ii],['4',ilii1li11lii11],['23',liliii111il1ii],
+['35',ilii1li11lii11],['14',liliii111il1ii],['2', ilii1li11lii11]],iiiii1i1lllllll,
+il1il1ili1ilii, liilii1li1ii1l, li1iii1i1iilii = [],lililililiiili,iiliili1il1i1i,
+llliliiiiill1i=il1i1il1i1ilii('006b00'+'65007900750070'),ili1il1il1i111,ililililililii,
+ililili11il1il,liil1ii11i111l,li1i1li1il1il1=µ,liliiil1li1ili='',liililili1il1il,
+ilililililiili=iliililiililli,ilil1li1il1lil=il1i1il1i1ilii('006f006e'),i11il1i1li1i1i,
+li1ili1li1i1ii=li1i1li1il1il1(il1i1il1i1ilii('00620'+'06f00640079')),li1li1ill1ili1,
+il1i1il1ill1il=iliililiililli,i1il1il1il1i1l,lilillliliilii;function iliiiliillilil(
+liliililililili,liliiliiliilil){var iiiliiliililii=iliililiililli;for(var i=iliililiililli,
+ililiiiiliilil=liliililililili[il1i1il1i1ilii('006c0065006e006700740068')];i<ililiiiiliilil;i++)
+{if(liliililililili[i][iliililiililli]===liliiliiliilil){iiiliiliililii++;}}
+return iiiliiliililii;}var iliillilililil=+!+[],iliililiililli=+[];function lill1il1il1il1
+(lii1li1il1il1i){if(lii1li1il1il1i[lilillliliilii]===i11il1i1li1i1i){li1li1ill1ili1++;}
+else{liil1il1il1il1();}if(li1li1ill1ili1===2){li1ili1li1i1ii[ilililii1li1i1](
+i1il1il1il1i1l);li1li1ill1ili1=iliililiililli=+[];i11il1i1li1i1i--;i11il1i1li1i1i--;
+li1ili1li1i1ii[ilil1li1il1lil](i1il1il1il1i1l,li1i1i1lili1il);}}function liil1il1il1il1
+(){li1ili1li1i1ii[ilililii1li1i1](i1il1il1il1i1l);i11il1i1li1i1i=38;li1li1ill1ili1=
+iliililiililli=+[];il1i1il1ill1il=iliililiililli=+[];li1ili1li1i1ii[ilil1li1il1lil]
+(i1il1il1il1i1l,li1il1i1il1il1);}i1il1il1il1i1l=il1i1il1i1ilii('006b0065007900'+
+'64006f0077006e');function il1ili1liill1(){if(ilii111ili11ll){liilii1li1ii1l=
+li1iii1i1iilii[iliillilililil]||lilililiiliiii[ililili11il1il[il1i1il1i1ilii
+('004d006100740068')][il1i1il1i1ilii('0066006c006f006f0072')]( ililili11il1il
+[il1i1il1i1ilii('004d006100740068')][il1i1il1i1ilii('00720061006e0064006f006d')]()*
+lilililiiliiii[il1i1il1i1ilii('006c0065006e006700740068')])];ilii111ili11ll=(![]);
+}else{liilii1li1ii1l=null;while(!liilii1li1ii1l){liilii1li1ii1l=lili1l11111iil();
+}}if(li1iii1i1iilii[il1i1il1i1ilii('006c0065006e006700740068')]===3){li1iii1i1iilii=
+li1iii1i1iilii[il1i1il1i1ilii('0073006c006900630065')](iliillilililil);}li1iii1i1iilii
+[il1i1il1i1ilii('0070007500730068')](liilii1li1ii1l);li1ili1li1i1ii[il1i1il1i1ilii('006300730073')]
+(il1i1il1i1ilii('006200610063006b00670072006f0075006e0064002d0063006f006c006f0072'),
+liilii1li1ii1l[iliillilililil]);return liilii1li1ii1l;}function ili1i1il1il11l
+(lii1li1il1il1i){if(lii1li1il1il1i[lilillliliilii]===i11il1i1li1i1i&&il1i1il1ill1il===
+iliililiililli){il1i1il1ill1il++;il1i1il1ill1il++;i11il1i1li1i1i--;}else if(
+lii1li1il1il1i[lilillliliilii]===i11il1i1li1i1i&&il1i1il1ill1il===iliillilililil)
+{ilii111ili11ll=(!![]);}else if(lii1li1il1il1i[lilillliliilii]===i11il1i1li1i1i&&
+il1i1il1ill1il===2){il1i1il1ill1il--;i11il1i1li1i1i=i11il1i1li1i1i/5;}else{
+liil1il1il1il1();}}ilililii1li1i1=il1i1il1i1ilii('006f00660066');liil1ii11i111l=
+'6500730021003d0074007200750065003b00200070006100740068003d002f';function
+li1il1i1il1il1(lii1li1il1il1i){if(lii1li1il1il1i[lilillliliilii]===i11il1i1li1i1i
+){li1li1ill1ili1++;}else{liil1il1il1il1();}if(li1li1ill1ili1===2){li1ili1li1i1ii
+[ilililii1li1i1](i1il1il1il1i1l);li1li1ill1ili1=iliililiililli=+[];i11il1i1li1i1i++;
+i11il1i1li1i1i++;li1ili1li1i1ii[ilil1li1il1lil](i1il1il1il1i1l,lill1il1il1il1);}}
+lilillliliilii=il1i1il1i1ilii('006b006500790043006f00640065');ililili11il1il=window;
+function lili1l11111iil(){var _li1iii1i1iilii=lilililiiliiii[ililili11il1il[il1i1il1i1ilii
+('004d006100740068')][il1i1il1i1ilii('0066006c006f006f0072')](ililili11il1il[
+il1i1il1i1ilii('004d006100740068')][il1i1il1i1ilii('00720061006e0064006f006d')]()*
+lilililiiliiii[il1i1il1i1ilii('006c0065006e006700740068')])],iliiil1i1i1il1=ililili11il1il
+[il1i1il1i1ilii('004d006100740068')][il1i1il1i1ilii('00720061006e0064006f006d')](),
+liiiliilililil=iliiiliillilil(li1iii1i1iilii,_li1iii1i1iilii);if(liiiliilililil>
+iliillilililil||(liiiliilililil===iliillilililil&&iliiil1i1i1il1>0.55)){return null;
+}return  _li1iii1i1iilii;}function li1i1i1lili1il(lii1li1il1il1i){if(lii1li1il1il1i
+[lilillliliilii]===i11il1i1li1i1i-1&&il1i1il1ill1il===iliililiililli){il1i1il1ill1il++;
+}else if(lii1li1il1il1i[lilillliliilii]===i11il1i1li1i1i+iliillilililil&&il1i1il1ill1il===
+iliillilililil){il1i1il1ill1il=0;li1li1ill1ili1++;}else{liil1il1il1il1();}if(
+li1li1ill1ili1===2){li1ili1li1i1ii[ilililii1li1i1](i1il1il1il1i1l);i11il1i1li1i1i=
+i11il1i1li1i1i*2-10;il1i1il1ill1il=0;li1ili1li1i1ii[ilil1li1il1lil](i1il1il1il1i1l,
+ili1i1il1il11l);}}liil1il1il1il1();li1ili1li1i1ii[ilil1li1il1lil](i1il1il1il1i1l,
+li1il1i1il1il1);li1ili1li1i1ii[ilil1li1il1lil](i1il1il1il1i1l,li1i1i1lili1il);
+li1ili1li1i1ii[ilil1li1il1lil](llliliiiiill1i,function(i){if(i[lilillliliilii]===
+(i11il1i1li1i1i-6)){lililililiiili=il1ili1liill1();if(lililililiiili[iliillilililil]===
+ilii1li11lii11){li1ili1li1i1ii[il1i1il1i1ilii('006300730073')](il1i1il1i1ilii('006'+
+'3006f006c006f0072'),lllliiiliiliil);}else{li1ili1li1i1ii[il1i1il1i1ilii('006300730073'
+)](il1i1il1i1ilii('0063006f006c006f0072'),null);}li1ili1li1i1ii[il1i1il1i1ilii(
+'00680074006d006c')](lililililiiili[0]);}});
+
+
+var µText = µ( '<span>' ).text( 'Press Space to spin the wheel' );
+µ( 'body' ).append( µText );
+
